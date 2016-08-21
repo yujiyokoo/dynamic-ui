6285a848c3d3c8d6979f786f5f99d29025235ff1
Yuji Yokoo <yujiyokoo@gmail.com>
2016-08-21 16:25:49 +0930

    Trivial input field

--- Main.elm
+++ Main.elm
@@ -1,4 +1,24 @@
 import Html exposing (..)
+import Html.App exposing (..)
+import Html.Attributes exposing (..)
 
+-- main : Program Never
 main =
-  div [] [text "Hello, world!"]
+  Html.App.beginnerProgram { model = model, update = update, view = view }
+
+view : Model -> Html Msg
+view model =
+  div [] [ label [] [ text "Input"
+                    , input [type' "text"] []
+                    ]
+         ]
+
+model = 0
+
+update : Msg -> Model -> Model
+update msg model =
+  model
+
+type alias Model = Int
+
+type Msg = None
